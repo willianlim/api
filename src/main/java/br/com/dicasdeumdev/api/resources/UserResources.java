36d2a1cb0f5349ba8efd8ae1eea46530e163c897
willianlim <willianrosendodelima@outlook.com>
2022-12-17 14:02:44 -0300

    class user resources findById

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResources.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResources.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResources {
 
+    @Autowired
+    UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "Valdir", "Valdir@gmail.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 
 }
