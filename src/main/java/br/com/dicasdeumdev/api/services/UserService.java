6195abef1baa66adc19778cfcba2fc323cf69c73
willianlim <willianrosendodelima@outlook.com>
2023-01-28 12:16:18 -0300

    A new beginning

--- src/main/java/br/com/dicasdeumdev/api/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package br.com.dicasdeumdev.api.services;
-
-import br.com.dicasdeumdev.api.domain.User;
-
-public interface UserService {
-
-    User findById(Integer id);
-
-}
--- src/main/java/br/com/dicasdeumdev/api/services/exception/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.dicasdeumdev.api.services.exception;
-
-public class ObjectNotFoundException extends RuntimeException {
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
