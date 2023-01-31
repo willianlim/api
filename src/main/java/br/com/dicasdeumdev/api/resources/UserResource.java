dbcec1969d9e57d2600e2c9ab8ddc0f330ba91ac
willianlim <willianrosendodelima@outlook.com>
2023-01-31 16:05:10 -0300

    endpoint findall para user

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -24,5 +26,12 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> list = userService.findAll();
+        List<UserDTO> listDTO = list.stream().map(x -> mapper.map(x, UserDTO.class)).toList();
+        return (ResponseEntity.ok().body(listDTO));
     }
 }
