19e4bc48504d246e9411f4ad9a93d511eb0e96ac
willianlim <willianrosendodelima@outlook.com>
2023-01-31 17:09:34 -0300

    endpoint update para user

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -40,4 +40,11 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
         return (ResponseEntity.created(uri).build());
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
+        obj.setId(id);
+        User newObj = userService.update(obj);
+        return ResponseEntity.ok().body(mapper.map(newObj, UserDTO.class));
+    }
 }
--- src/main/java/br/com/dicasdeumdev/api/services/Implementation/UserServiceImplementation.java
+++ src/main/java/br/com/dicasdeumdev/api/services/Implementation/UserServiceImplementation.java
@@ -38,9 +38,15 @@ public class UserServiceImplementation implements UserService {
         return userRepository.save(mapper.map(obj, User.class));
     }
 
+    @Override
+    public User update(UserDTO obj) {
+        findByEmail(obj);
+        return userRepository.save(mapper.map(obj, User.class));
+    }
+
     private void findByEmail(UserDTO obj) {
         Optional<User> user = userRepository.findByEmail(obj.getEmail());
-        if (user.isPresent()) {
+        if (user.isPresent() && !user.get().getId().equals(obj.getId())) {
             throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
         }
     }
